73ef6daa883e5ab4727e59718309d823c954a871
Roxiq Mulna Muhammad <roxiqmulna02@gmail.com>
2022-06-27 23:41:54 +0700

    add CRUD. but error on update and delete

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const methodOverride = require("method-override");
+
 // import mongoose
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/db_staycation", {
@@ -23,7 +25,7 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.use(methodOverride("method"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
