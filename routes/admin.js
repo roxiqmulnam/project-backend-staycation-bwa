457ca3f250ad477375d5cbc2ff67c308ed6f780f
Roxiq Mulna Muhammad <roxiqmulna02@gmail.com>
2022-06-29 22:40:25 +0700

    add feature upload image, add and edit modal

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const adminController = require("../controllers/adminController");
+const { upload } = require('../middlewares/multer')
 
 router.get("/dashboard", adminController.viewDashboard);
 
@@ -9,6 +10,10 @@ router.put("/category", adminController.editCategory);
 router.delete("/category/:id", adminController.deleteCategory);
 
 router.get("/bank", adminController.viewBank);
+router.post("/bank", upload, adminController.addBank);
+router.put("/bank", upload, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
+
 router.get("/item", adminController.viewItem);
 router.get("/booking", adminController.viewBooking);
 
