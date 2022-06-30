0aaf654d3972f3b260beb59262da65972c7a55a1
Roxiq Mulna Muhammad <roxiqmulna02@gmail.com>
2022-06-30 23:50:28 +0700

    add CRUD items linked from category, can upload image multiple and view image details

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const adminController = require("../controllers/adminController");
-const { upload } = require('../middlewares/multer')
+const { uploadSingle, uploadMultiple } = require("../middlewares/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
 
@@ -10,11 +10,18 @@ router.put("/category", adminController.editCategory);
 router.delete("/category/:id", adminController.deleteCategory);
 
 router.get("/bank", adminController.viewBank);
-router.post("/bank", upload, adminController.addBank);
-router.put("/bank", upload, adminController.editBank);
+router.post("/bank", uploadSingle, adminController.addBank);
+router.put("/bank", uploadSingle, adminController.editBank);
 router.delete("/bank/:id", adminController.deleteBank);
 
+
 router.get("/item", adminController.viewItem);
+router.post("/item", uploadMultiple, adminController.addItem);
+router.get("/item/show-image/:id", adminController.showImageItem);
+router.get("/item/:id", adminController.showEditItem);
+router.put('/item/:id', uploadMultiple, adminController.editItem);
+router.delete("/item/:id/delete", adminController.deleteItem);
+
 router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
