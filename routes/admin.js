5b725d1022a77876746d50d0fa2886fc4336eaac
Roxiq Mulna Muhammad <roxiqmulna02@gmail.com>
2022-07-01 23:32:20 +0700

    add show item detail from item listed, and not working properly

--- routes/admin.js
+++ routes/admin.js
@@ -22,6 +22,10 @@ router.get("/item/:id", adminController.showEditItem);
 router.put('/item/:id', uploadMultiple, adminController.editItem);
 router.delete("/item/:id/delete", adminController.deleteItem);
 
+router.get('/item/show-detail-item/:itemId', adminController.viewDetailItem);
+router.post('/item/add/feature', uploadSingle, adminController.addFeature);
+router.put('/item/update/feature', uploadSingle, adminController.editFeature);
+
 router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
