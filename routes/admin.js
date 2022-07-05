b0c5fb9a3fdba3f373f38b1a50ed73001f45c154
Roxiq Mulna Muhammad <roxiqmulna02@gmail.com>
2022-07-05 10:36:01 +0700

    add CRUD Show Activity

--- routes/admin.js
+++ routes/admin.js
@@ -20,11 +20,16 @@ router.get("/item/show-image/:id", adminController.showImageItem);
 router.get("/item/:id", adminController.showEditItem);
 router.put('/item/:id', uploadMultiple, adminController.editItem);
 router.delete("/item/:id/delete", adminController.deleteItem);
-// DETAIL ITEM 
+// DETAIL ITEM
 router.get('/item/show-detail-item/:itemId', adminController.viewDetailItem);
+// DETAIL ITEM FEATURE
 router.post('/item/add/feature', uploadSingle, adminController.addFeature);
 router.put('/item/update/feature', uploadSingle, adminController.editFeature);
 router.delete('/item/:itemId/feature/:id', adminController.deleteFeature);
+// DETAIL ITEM ACTIVITY 
+router.post('/item/add/activity', uploadSingle, adminController.addActivity);
+router.put('/item/update/activity', uploadSingle, adminController.editActivity);
+router.delete('/item/:itemId/activity/:id', adminController.deleteActivity);
 
 router.get("/booking", adminController.viewBooking);
 
