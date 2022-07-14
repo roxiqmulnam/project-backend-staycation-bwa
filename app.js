9c19ae0b889565b141ad5544c0f252202e9882dd
Roxiq Mulna Muhammad <roxiqmulna02@gmail.com>
2022-07-14 23:47:53 +0700

    create API Landing Page

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var usersRouter = require("./routes/users");
 
 // import router
 const adminRouter = require("./routes/admin");
+const apiRouter = require("./routes/api");
 
 var app = express();
 
@@ -48,7 +49,9 @@ app.use(
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+// admin
 app.use("/admin", adminRouter);
+app.use("/api/v1/member", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
